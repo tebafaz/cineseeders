6916f847114d18224478135a5f54ec39993c6cdc
tebafaz <tebafaz@gmail.com>
2023-03-25 22:28:33 +0600

    fixed error with document head

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,20 +3,15 @@ import '../../styles/globals.css'
 
 import Footer from "../components/footer"
 import Navbar from '../components/navbar'
-import Head from 'next/head'
+import { HeadDoc } from '../components/head'
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Head>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossOrigin="anonymous"></link>
-      </Head>
-      <body>
-        <Navbar />
-        <Component {...pageProps} />
-        <Footer />
-      </body>
+      <HeadDoc />
+      <Navbar />
+      <Component {...pageProps} />
+      <Footer />
     </>
   )
 }
